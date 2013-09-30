5e2a97703e5aa4956a9054d70efd157f7c614ff3
Matt Hoover <mhoover@ncsasports.org>
2013-09-30 15:50:56 -0500

    make create_board private

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,14 +5,6 @@ class Game
     @board = create_board(tiles)
   end
 
-  def create_board(tiles)
-    size = Math.sqrt(tiles.length)
-    board = Board.new(size.to_i)
-    board.tiles = tiles
-    board.move_count = board.count_moves
-    board
-  end
-
   def computer_move
     unless @board.game_over?
       if @board.move_count.odd?
@@ -20,5 +12,15 @@ class Game
         computer.move
       end
     end
+  end
+
+  private
+
+  def create_board(tiles)
+    size = Math.sqrt(tiles.length)
+    board = Board.new(size.to_i)
+    board.tiles = tiles
+    board.move_count = board.count_moves
+    board
   end
 end
